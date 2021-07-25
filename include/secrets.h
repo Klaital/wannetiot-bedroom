67888ac3de92f9ae4e3114f1a2e5d3a3a67a83f9
Kit Cox <chriscox@abandonedfactory.net>
2021-07-24 18:46:31 -0700

    Set the timestamp on data points

--- include/secrets.h
+++ include/secrets.h
@@ -5,11 +5,12 @@
 #ifndef WANNETIOT_BEDROOM_SECRETS_H
 #define WANNETIOT_BEDROOM_SECRETS_H
 
-#define SECRET_SSID ""
-#define SECRET_PASS ""
-#define INFLUX_HOST ""
-#define INFLUX_TOKEN ""
-#define INFLUX_ORG ""
-#define INFLUX_BUCKET ""
+#define SECRET_SSID "WANNET-IOT"
+#define SECRET_PASS "AT%*#2Aa6tj9^kfWclWt^U2!8S81*sl@"
+#define INFLUX_HOST "klaital.com"
+#define INFLUX_PORT 8086
+#define INFLUX_TOKEN "o61F__kGOJuOSl7MST2pYNf3p23lBXHSrmnfq2inOF4IRwZXekFEStp4-NZ-2eHuuZihP1elg5w5egZ1EbDbfg=="
+#define INFLUX_ORG "klaital.com"
+#define INFLUX_BUCKET "wannetiot"
 
 #endif //WANNETIOT_BEDROOM_SECRETS_H
